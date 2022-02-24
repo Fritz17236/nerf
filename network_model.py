3e7c976854264c79db2568baf72603b93fe3936e
Fritz17236 <fritz17236@hotmail.com>
2022-02-23 23:02:11 -0800

    update, working but need to train net with stratified sampling, also fixed bug where rays_d and rays_o were switched before rendering image. New bug:network occationally starts off with zero sigma output, leading to no progress in training / deadhang. Need to adjust weights/ initialize so that this doesn't happen

--- network_model.py
+++ network_model.py
@@ -7,7 +7,7 @@ import wget
 from warnings import warn
 import constants as const
 import skimage
-import time
+import time, datetime
 from numba import jit
 
 
@@ -81,18 +81,18 @@ class NerfNet(nn.Module):
         # create a sequential stack that takes (network_input,y,z) input in batches, process first 4 hidden layers
         layer_list = [nn.Linear(3 + 3 * 2 * const.L_ENCODE, 256), nn.ReLU()]
         for i in range(3):
-            layer_list += [nn.Linear(256, 256), nn.ReLU()]
+            layer_list += [nn.Linear(256, 256, bias=False), nn.ReLU()]
         self.stack_pre_inject = nn.Sequential(*layer_list)
 
         # inject input again and process 4 remaining layers
         layer_list = [nn.Linear(3 + 3 * 2 * const.L_ENCODE + 256, 256), nn.ReLU()]
         for i in range(3):
-            layer_list += [nn.Linear(256, 256), nn.ReLU()]
+            layer_list += [nn.Linear(256, 256, bias=False), nn.ReLU()]
         self.stack_post_inject = nn.Sequential(*layer_list)
 
         # TODO: implement directional (not just density) encoding in network
 
-        self.out_layer = nn.Sequential(*[nn.ReLU(), nn.Linear(256, 128), nn.Linear(128, 4)])
+        self.out_layer = nn.Sequential(*[nn.ReLU(), nn.Linear(256, 128, bias=False), nn.Linear(128, 4)])
 
     def forward(self, network_input):
         network_input = self.embed(network_input)
@@ -128,7 +128,7 @@ def compute_sample_rays(height: int, width: int, focal_len: float, c2w: torch.Te
 
     # create copies of ray origin to match the size / number of ray directions
     rays_o = torch.broadcast_to(c2w[:3, -1], rays_d.size())
-    return rays_d, rays_o
+    return rays_o, rays_d
 
 
 def render_rays(model: NerfNet(), rays_o: torch.Tensor, rays_d: torch.Tensor,
@@ -143,14 +143,14 @@ def render_rays(model: NerfNet(), rays_o: torch.Tensor, rays_d: torch.Tensor,
     :param far: float specifying the far clipping plane of the view frustum
     :param num_samples: number of points along each cast ray to query the network
     :param stratified_sampling: uniformly sample query points along each ray
-    :return: rgbs, depths,
+    :return: rgbs
     """
 
     # compute 3D query points
     ts = torch.linspace(start=near, end=far, steps=num_samples).type(const.DTYPE)
     if stratified_sampling:
         add = torch.rand(list(rays_o.size()[:-1]) + [num_samples]) * (far - near) / (num_samples)
-        ts = ts[np.newaxis, np.newaxis, :] + add
+        ts = ts[np.newaxis, np.newaxis, :] + add.type(const.DTYPE)
     query_points = rays_o[..., None, :] + rays_d[..., None, :] * ts[..., :, None]
 
     # Extract value of network at query points
@@ -217,6 +217,8 @@ if __name__ == '__main__':
 
     # enter training loop
     t_start = time.time()
+
+    print("training start at: ", datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
     for idx_epoch in range(const.NUM_TRAINING_EPOCHS):
         psnrs = []
         for i, (target_img, pose_in) in enumerate(torch.utils.data.DataLoader(ip_dataset)):
@@ -233,142 +235,12 @@ if __name__ == '__main__':
                                                         output_img.cpu().detach().numpy())
             )
         seconds = time.time() - t_start
-        time_str = "{0}:{1}:{2}".format(int(seconds // (60 * 60)), int((seconds // 60) % 60),  int(seconds % 60))
+        time_str = "{0:0>2}:{1:0>2}:{2:0<2}".format(int(seconds // (60 * 60)), int((seconds // 60) % 60),  int(seconds % 60))
 
-        print('epoch: {0}, psnr: {1}, time: {2}'.format(idx_epoch, torch.mean(torch.tensor(psnrs)), time_str))
+        print('epoch: {0:0>5}, psnr: {1:>8}, loss: {3:>5},  elapsed time: {2}'.format(idx_epoch, np.round(torch.mean(torch.tensor(psnrs)).cpu().detach().numpy(),6), time_str, loss.item()))
         torch.save(model.state_dict(), os.path.join(const.MODEL_SAVE_DIR, "nerf_net"))
 
     # exit(0)
 
 # endregion
 
-# region old code
-# # Download training data from open datasets.
-# training_data = datasets.FashionMNIST(
-#     root="data",
-#     train=True,
-#     download=True,
-#     transform=ToTensor(),
-# )
-#
-# # Download test data from open datasets.
-# test_data = datasets.FashionMNIST(
-#     root="data",
-#     train=False,
-#     download=True,
-#     transform=ToTensor(),
-# )
-#
-# batch_size = 64
-#
-# # Create data loaders.
-# train_dataloader = DataLoader(training_data, batch_size=batch_size)
-# test_dataloader = DataLoader(test_data, batch_size=batch_size)
-#
-# for X, y in test_dataloader:
-#     print(f"Shape of X [N, C, H, W]: {X.shape}")
-#     print(f"Shape of y: {y.shape} {y.dtype}")
-#     break
-#
-# # Get cpu or gpu device for training.
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-# print(f"Using {device} device")
-#
-#
-# # Define model
-# class NeuralNetwork(nn.Module):
-#     def __init__(self):
-#         super(NeuralNetwork, self).__init__()
-#         self.flatten = nn.Flatten()
-#         self.linear_relu_stack = nn.Sequential(
-#             nn.Linear(28 * 28, 512),
-#             nn.ReLU(),
-#             nn.Linear(512, 512),
-#             nn.ReLU(),
-#             nn.Linear(512, 10)
-#         )
-#
-#     def forward(self, x):
-#         x = self.flatten(x)
-#         logits = self.linear_relu_stack(x)
-#         return logits
-#
-#
-# model = NeuralNetwork().to(device)
-# print(model)
-#
-# loss_fn = nn.CrossEntropyLoss()
-# optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-#
-#
-# def train(dataloader, model, loss_fn, optimizer):
-#     size = len(dataloader.dataset)
-#     model.train()
-#     for batch_num, (X, y) in enumerate(dataloader):
-#         X, y = X.to(device), y.to(device)
-#
-#         # Compute prediction error
-#         pred = model(X)
-#         loss = loss_fn(pred, y)
-#
-#         # Backpropagation
-#         optimizer.zero_grad()  # zeros gradient buffers
-#         loss.backward()  #
-#         optimizer.step()
-#
-#         if batch_num % 100 == 0:
-#             loss, current = loss.item(), batch_num * len(X)
-#             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-#
-#
-# def test(dataloader, model, loss_fn):
-#     size = len(dataloader.dataset)
-#     num_batches = len(dataloader)
-#     model.eval()
-#     test_loss, correct = 0, 0
-#     with torch.no_grad():  # if you know yhou won't comptue gradients, this speeds up computations
-#         for X, y in dataloader:
-#             X, y = X.to(device), y.to(device)
-#             pred = model(X)
-#             test_loss += loss_fn(pred, y).item()
-#             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-#     test_loss /= num_batches
-#     correct /= size
-#     print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-#
-#
-# epochs = 5
-# for t in range(epochs):
-#     print(f"Epoch {t + 1}\n-------------------------------")
-#     train(train_dataloader, model, loss_fn, optimizer)
-#     test(test_dataloader, model, loss_fn)
-# print("Done!")
-#
-# torch.save(model.state_dict(), "model.pth")
-# print("Saved PyTorch Model State to model.pth")
-#
-# model = NeuralNetwork()
-# model.load_state_dict(torch.load("model.pth"))
-#
-# classes = [
-#     "T-shirt/top",
-#     "Trouser",
-#     "Pullover",
-#     "Dress",
-#     "Coat",
-#     "Sandal",
-#     "Shirt",
-#     "Sneaker",
-#     "Bag",
-#     "Ankle boot",
-# ]
-#
-# model.eval()  # switch network to evaluate model mode
-#
-# x, y = test_data[0][0], test_data[0][1]
-# with torch.no_grad():
-#     pred = model(x)
-#     predicted, actual = classes[pred[0].argmax(0)], classes[y]
-#     print(f'Predicted: "{predicted}", Actual: "{actual}"')
-#
-# endregion
--- query_network.py
+++ query_network.py
@@ -2,46 +2,115 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+import tqdm
 from torch import nn
 import wget
 from warnings import warn
 import constants as const
 from network_model import NerfNet, ImagePoseDataset, compute_sample_rays, render_rays
+from matplotlib.widgets import Slider
 import skimage
 import time
 
-# Load Data
-data_file = os.path.join('data', 'tiny_nerf_data.npz')
-if not os.path.exists(data_file):  # get tiny nerf data
-    wget.download("http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz", out=data_file)
 
-data = np.load(data_file)
-images = torch.from_numpy(data['images']).type(const.DTYPE)
-poses = torch.from_numpy(data['poses']).type(const.DTYPE)
-focal = torch.from_numpy(data['focal']).type(const.DTYPE)  # set to foal_len = torch.from...
-H, W = images.shape[1:3]  # set to height, width = images.shape[1:3]
-ip_dataset = ImagePoseDataset(images, poses)
-near = const.NEAR_FRUSTUM  # remove
-far = const.FAR_FRUSTUM  # remove
+def pose_spherical(theta, phi, radius):
+    """
+    Given view direction specified by spherical coordinates (theta, phi, radius) return a 4x4 camera-to-world matrix
+    :param theta:
+    :param phi:
+    :param radius:
+    :return:
+    """
+    theta = torch.Tensor([theta])
+    phi = torch.tensor([phi])
+    radius = torch.tensor([radius])
 
-height = H  # remove
-width = W  # remove
-focal_len = focal  # remove
-num_samples = const.NUM_RAY_SAMPLES  # remove
-N_samples = num_samples  # remove
-L_embed = const.L_ENCODE  # remove
+    # transforms a point along the z axis by given t
+    transform_z = lambda t: torch.tensor([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, t],
+        [0, 0, 0, 1]
+    ]).type(const.DTYPE)
+
+    # rotates a point away from the y-z plane by given angle phi
+    rotate_phi = lambda phi: torch.tensor([
+        [1, 0, 0, 0],
+        [0, torch.cos(phi), -torch.sin(phi), 0],
+        [0, torch.sin(phi), torch.cos(phi), 0],
+        [0, 0, 0, 1]
+    ]).type(const.DTYPE)
+
+    # rotates a point away from the x-z plane
+    rotate_theta = lambda theta: torch.tensor([
+        [torch.cos(theta), 0, -torch.sin(theta), 0],
+        [0, 1, 0, 0],
+        [torch.sin(theta), 0, torch.cos(theta), 0],
+        [0, 0, 0, 1]
+    ]).type(const.DTYPE)
+
+    # compose the transformations above to compute the camera_to_world matrix (pose)
+    camera_to_world = transform_z(radius)
+    camera_to_world = rotate_phi(phi/180. * np.pi) @ camera_to_world
+    camera_to_world = rotate_theta(theta/180. * np.pi) @ camera_to_world
+    camera_to_world = torch.tensor([[-1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]).type(
+        const.DTYPE) @ camera_to_world  # ?
+
+    return camera_to_world
 
 
-model_file = os.path.join(const.MODEL_SAVE_DIR, 'nerf_net')
-model = NerfNet().type(const.DTYPE)
-if os.path.exists(model_file):
-    model.load_state_dict(torch.load(model_file))
+def render_view(**kwargs):
+    camera_to_world = pose_spherical(**kwargs)
+    rays_o, rays_d = compute_sample_rays(height, width, focal_len, camera_to_world)
+    output_img = render_rays(model, rays_o, rays_d)
+    img = torch.clip(output_img, min=0, max=1).cpu().detach().numpy()
+
+    plt.figure('network render')
+    plt.imshow(img)
+    plt.show()
+
+if __name__ == '__main__':
+
+    # Load Data
+    data_file = os.path.join('data', 'tiny_nerf_data.npz')
+    if not os.path.exists(data_file):  # get tiny nerf data
+        wget.download("http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz",
+                      out=data_file)
+
+    data = np.load(data_file)
+    images = torch.from_numpy(data['images']).type(const.DTYPE)
+    poses = torch.from_numpy(data['poses']).type(const.DTYPE)
+    focal_len = torch.from_numpy(data['focal']).type(const.DTYPE)  # set to foal_len = torch.from...
+    height, width = images.shape[1:3]  # set to height, width = images.shape[1:3]
 
 
-target_img = images[0,...]
-pose_in = poses[0,...]
-pose_in = torch.squeeze(pose_in)
-target_img = torch.squeeze(target_img)
-rays_o, rays_d = compute_sample_rays(height, width, focal_len, torch.squeeze(pose_in))
-model.zero_grad()  # zero gradient buffer before rendering rays
-output_img = render_rays(model, rays_o, rays_d)
+    # theta, phi, radius provide these to render to perform rendering
+    model = NerfNet().type(const.DTYPE)
+    model_file = os.path.join(const.MODEL_SAVE_DIR, 'nerf_net')
+    if os.path.exists(model_file):
+        model.load_state_dict(torch.load(model_file))
+
+
+
+kwargs = {"theta": 0, "phi": 0, "radius": 1}
+render_view(**kwargs)
+
+frames = []
+interp_rate = 100
+torch.cuda.empty_cache()
+
+
+
+frames = []
+for th in tqdm.tqdm(np.linspace(0., 360., 120, endpoint=False)):
+    c2w = pose_spherical(th, -30., 4.)
+    rays_o, rays_d = compute_sample_rays(height, width, focal_len, c2w[:3,:4])
+    rgb = render_rays(model, rays_o, rays_d,).cpu().detach().numpy()
+    frames.append((255*np.clip(rgb, 0 ,1)).astype(np.uint8))
+
+import imageio
+f = 'video.mp4'
+imageio.mimwrite(f, frames, fps=30, quality=7)
+
+kwargs = {"theta": 0, "phi": 0, "radius": 1}
+render_view()
